2be531987b7227a2fbdc45f198df0dc84af713c9
Andrey Antakov <andrey.antakov05@gmail.com>
2024-03-20 13:14:48 +0300

    client_page_collapse_orderList_added

--- frontend/src/pages/ClientPage/ClientPage.jsx
+++ frontend/src/pages/ClientPage/ClientPage.jsx
@@ -1,9 +1,83 @@
-import {Card, Timeline, List} from "antd";
+import {Card, Timeline, List,Collapse} from "antd";
 import { useParams, Link } from "react-router-dom";
 
 function ClientPage()
 {
+    const items = [
+        {
+          key: '1',
+          label: 'This is panel header 3',
+          children: <div>
+            <Timeline
+                items={[
+                {
+                    children: 'Create a services site 2015-09-01',
+                },
+                {
+                    children: 'Solve initial network problems 2015-09-01',
+                },
+                {
+                    children: 'Technical testing 2015-09-01',
+                },
+                {
+                    
+                    children: 'Network problems being solved 2015-09-01',
+                },
+                ]}
+            />
+            <Link to={"/order/"+1}>Подробнее</Link>
+          </div>
+        },
+        {
+          key: '2',
+          label: 'This is panel header 3',
+          children: <div>
+            <Timeline
+                items={[
+                {
+                    children: 'Create a services site 2015-09-01',
+                },
+                {
+                    children: 'Solve initial network problems 2015-09-01',
+                },
+                {
+                    children: 'Technical testing 2015-09-01',
+                },
+                {
+                    children: 'Network problems being solved 2015-09-01',
+                },
+                ]}
+            />
+            <Link to={"/order/"+1}>Подробнее</Link>
+          </div>
+        },
+        {
+          key: '3',
+          label: <div style={{display:"flex",justifyContent:"space-between",marginTop:"0px"}}><p>Заказ № 321</p><p>Создан: 12.01.2023</p></div>,
+          children: <div>
+            <Timeline
+                items={[
+                {
+                    children: 'Create a services site 2015-09-01',
+                },
+                {
+                    children: 'Solve initial network problems 2015-09-01',
+                },
+                {
+                    children: 'Technical testing 2015-09-01',
+                },
+                {
+                    children: 'Network problems being solved 2015-09-01',
+                },
+                ]}
+            />
+            <Link to={"/order/"+1}>Подробнее</Link>
+          </div>
+        },
+      ];
+
     const {id} = useParams();
+
     return(
         <>
             <h3>РУП НЦОТ</h3>
@@ -13,29 +87,10 @@ function ClientPage()
                 <p>УНП:123754234</p>
             </div>
             <h3 style={{textAlign:"center"}}>Актуальные заказы</h3>
-            <Card title={<><p>Заказ № {id}</p><p>12.01.2023</p></>}>
-                <Timeline
-                    items={[
-                    {
-                        children: 'Create a services site 2015-09-01',
-                    },
-                    {
-                        children: 'Solve initial network problems 2015-09-01',
-                    },
-                    {
-                        children: 'Technical testing 2015-09-01',
-                    },
-                    {
-                        children: 'Network problems being solved 2015-09-01',
-                    },
-                    ]}
-                />
-                <p>Создан: 12.01.2023</p>
-                <Link to={"/order/"+id}>Подробнее</Link>
-            </Card>
-            <List>
-
-            </List>
+            <Collapse
+                defaultActiveKey={['1']}
+                items={items}
+            />
         </>
     )
 }
