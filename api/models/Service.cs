a4f02e9c64f7d4dc6e99d019a54c4c70d554474d
Andrey Antakov <andrey.antakov05@gmail.com>
2024-03-24 09:59:17 +0300

    service_controller_add

--- api/models/Service.cs
+++ api/models/Service.cs
@@ -7,6 +7,6 @@
 		public double Cost { get; set; }
 		public string Description { get; set; }
 		public int CategoryId { get; set; }
-		public string CategoryName { get; set; }
+		public Category Category { get; set; }
 	}
 }
